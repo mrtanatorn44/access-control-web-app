69a8b21d256529c8264d1e7aa9baa66686e57a6b
mrtanatorn44 <mrtanatorn44@gmail.com>
2022-10-09 22:58:12 +0700

    Add page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Recovery from "./components/Recovery";
 import Register from './components/Register';
 import Profile from "./components/Profile";
 import Verify from "./components/Verify";
-import ResetPassword from "./components/ResetPassword";
+import Reset from "./components/Reset";
 
 export default function App() {
   return (
@@ -17,7 +17,7 @@ export default function App() {
         <Route path="/recovery" element={<Recovery />} />
         <Route path="/register" element={<Register />} />
         <Route path="/verify" element={<Verify />} />
-        <Route path="/resetPassword" element={<ResetPassword />} />
+        <Route path="/reset" element={<Reset />} />
       </Routes>
     </BrowserRouter>
   );
