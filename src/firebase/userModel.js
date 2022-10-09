f548c7757006d9538792ff311f5e1c23ccf1d4d1
mrtanatorn44 <mrtanatorn44@gmail.com>
2022-10-10 04:06:12 +0700

    Add loging system with firebase

--- src/firebase/userModel.js
+++ src/firebase/userModel.js
@@ -1,4 +1,4 @@
-import { collection, addDoc ,getFirestore, getDocs, updateDoc, doc, query, where, deleteDoc  } from "firebase/firestore"; 
+import { collection, addDoc ,getFirestore, getDocs, updateDoc, doc, query, where, deleteDoc, orderBy, deleteField } from "firebase/firestore"; 
 import app from "./Connect";
 import bcrypt from 'bcryptjs'
 const db = getFirestore(app);
@@ -37,15 +37,17 @@ export async function addUser(dataUser, success ,unsuccess){
       }
       const docRef =  addDoc(collection(db, "Users"), user_data)
       .then((doc) => {
-        console.log("add User success!");
+        saveLog('INFO', `Username: ${user_data.username} has registered.`)
         return success(user_data)
       })
       .catch((error) => {
-        console.error("add User  error ", error);
+        saveLog('ERROR', `FUNC(addUser) => ${error}`)
+        return unsuccess('Error')
       });
     }
-  } catch (e) {
-    console.error("Error adding document: ", e);
+  } catch (error) {
+    saveLog('ERROR', `FUNC(addUser) => ${error}`)
+    return unsuccess('Error')
   }
 }
 
@@ -69,22 +71,27 @@ export async function Login(loginForm, loginSuccess, loginFailed, emailNotVerify
           if (user.email_verify) {
             // Check password expire
             if (user.time_password.toDate().getTime() < new Date().getTime()) {
+              saveLog('INFO', `Username: ${user.username} try to login with expired password.`)
               return expiredPassword(user)
             } 
             // No exception
             else {
+              saveLog('INFO', `Username: ${user.username} has logged-in.`)
               return loginSuccess(user)
             }
           } else {
+            saveLog('INFO', `Username: ${user.username} try to login with un-verify email.`)
             return emailNotVerify(user)
           }
         } else {
+          saveLog('INFO', `Username: ${user.username}  try to login with wrong password.`)
           return loginFailed('wrong password')
         }
       }
     });
-  } catch (e) {
-    console.error("Login Failed", e);
+  } catch (error) {
+    saveLog('ERROR', `FUNC(Login) => ${error}`)
+    return loginFailed('Error')
   }
 }
 
@@ -108,13 +115,14 @@ export async function sendResetPw(email, sendResetPwSuccess, sendResetPwUnsucces
             reset_hash   : new_reset_hash,
             reset_time   : new Date(new Date().getTime() + 30*60000) // add 30 minutes since create acc
           }))
+          saveLog('INFO', `Send reset password link to ${user.email} (${user.username}).`)
           user.reset_hash = new_reset_hash;
           return sendResetPwSuccess(user)
         }
       });
     }
   } catch (error) {
-    console.error("sendForgetPassword : ", error);
+    saveLog('ERROR', `FUNC(sendResetPw) => ${error}`)
     return sendResetPwUnsuccess('Error')
   }
 }
@@ -132,15 +140,17 @@ export async function verifyResetPw(reset_hash, verifySuccess, verifyUnsuccess) 
         // Check hash verify
         if (user.reset_hash === reset_hash) { 
           if (user.reset_time.toDate().getTime() < new Date().getTime()) {
+            saveLog('INFO', `Username: ${user.username} reset link expired.`)
             return verifyUnsuccess("Reset link expired.")
           } else {
+            saveLog('INFO', `Username: ${user.username} reset link granted.`)
             return verifySuccess(user)
           }
         }
       });
     }
   } catch (error) {
-    console.error("verifyEmail : ", error);
+    saveLog('ERROR', `FUNC(verifyResetPw) => ${error}`)
     return verifyUnsuccess('Error')
   }
 }
@@ -156,7 +166,6 @@ export async function updatePassword(userData,password, updateSuccess, updateUns
       querySnapshot.forEach((user) => {
         const user_ID = user.id
         user = user.data()
-        ///
         var salt_pw = user.salt_password
         var hash_pw = bcrypt.hashSync(password, salt_pw);
         console.log(user.hash_password);
@@ -170,16 +179,16 @@ export async function updatePassword(userData,password, updateSuccess, updateUns
             reset_hash    : '',
             reset_time    : ''
           }))
-            
+          saveLog('INFO', `Username:${user.username} has update password.`)
           return updateSuccess()
         }else{
+          saveLog('INFO', `Username:${user.username} update password not success, password same as old password.`)
           return updateUnsuccess()
-          
         }
       });
     }
   } catch (error) {
-    console.error(error)
+    saveLog('ERROR', `FUNC(updatePassword) => ${error}`)
     return updateUnsuccess('Error')
   }
 }
@@ -198,20 +207,27 @@ export async function verifyEmail(hash, verifySuccess, verifyUnsuccess, verifyAl
         // Check hash verify
         if (user.verify_hash === hash) { 
           if (user.email_verify) {
+            saveLog('WARNING', `Username:${user.username} has already verify email but link still access.`)
             return verifyAlready(user.email)
           } else if (user.verify_time.toDate().getTime() < new Date().getTime()) {
+            saveLog('INFO', `Username:${user.username} has expired password.`)
             return verifyExpired({ email: user.email, hash: hash})
           } else {
             const  targetUserRef = doc(db, "Users", user_ID);
-            updateDoc(targetUserRef, ({ email_verify: true }))
+            updateDoc(targetUserRef, ({ 
+              email_verify  : true ,
+              verify_hash   : deleteField(),
+              verify_time   : deleteField()
+            }))
             deleteTargetEmail(user.email)
+            saveLog('INFO', `Username:${user.username} has succes verified email.`)
             return verifySuccess(user.email)
           }
         }
       });
     }
   } catch (error) {
-    console.error("verifyEmail : ", error);
+    saveLog('ERROR', `FUNC(verifyEmail) => ${error}`)
     return verifyUnsuccess('error')
   }
 }
@@ -232,17 +248,19 @@ export async function deleteTargetEmail(email) {
           const  targetUserRef = doc(db, "Users", user_ID);
           deleteDoc(targetUserRef)
             .then(() => {
-              console.log("User with email " + email + ' has delete docs')
+              // console.log("User with email " + email + ' has delete docs')
+              saveLog('INFO', `Delete doc in 'Users' by username:${user.username}, email:${email}`)
             })
             .catch(error => {
               console.log(error);
+              saveLog('ERROR', `FUNC(deleteTargetEmail) => ${error}`)
             })
           // return verifySuccess(user.email)
         }
       });
     }
   } catch (error) {
-    console.error("verifyEmail : ", error);
+    saveLog('ERROR', `FUNC(deleteTargetEmail) => ${error}`)
     // return verifyUnsuccess('error')
   }
 }
@@ -289,13 +307,47 @@ export async function sendVerify(hash, repeatSuccess, repeatUnsuccess) {
             verify_hash   : new_verify_hash,
             verify_time   : new Date(new Date().getTime() + 30*60000) // add 30 minutes since create acc
           }))
+          saveLog('INFO', `Send verify to username:${user.username}, email${user.email}`)
           user.verify_hash = new_verify_hash;
           return repeatSuccess(user)
         }
       });
     }
   } catch (error) {
-    console.error("verifyEmail : ", error);
+    saveLog('ERROR', `FUNC(sendVerify) => ${error}`)
     return repeatUnsuccess('Error')
+  }
+}
+
+export async function readLog(success) {
+  try {
+    const logRef = collection(db, "Logs");
+    const logQuery = query(logRef, orderBy("timestamp", "desc"));
+    const logSnapshot = await getDocs(logQuery);
+    var log_array = []
+    logSnapshot.forEach(log => {
+      log_array.push(log.data())
+    })
+    return success(log_array)
+
+  } catch (error) {
+  
+  }
+}
+export async function saveLog(type, text) {
+  try {
+    await addDoc(collection(db, "Logs"), {
+      type      : type,
+      text      : text,
+      timestamp : new Date(new Date().toLocaleString('en', {timeZone: 'Asia/Bangkok'}))
+    })
+      .then((doc) => {
+        // console.log("");
+      })
+      .catch((error) => {
+        console.error("error");
+      });
+  } catch (error) {
+    console.log("add log error");
   }
 }
